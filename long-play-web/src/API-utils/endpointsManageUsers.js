70c005b9cf4bed6ac342f393f3923212d1a13793
Artur Judycki <artur.judycki55@gmail.com>
2023-01-20 17:03:26 +0100

    finish user case managing users

--- long-play-web/src/API-utils/endpointsManageUsers.js
+++ long-play-web/src/API-utils/endpointsManageUsers.js
@@ -20,3 +20,41 @@ export const createEditorAdmin = async (values) => {
     });
   }
 };
+
+export const getEditorUsers = async (values) => {
+  const response = await fetch(
+    "".concat(`${base_url}`, "/user/getEditorUsers"),
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    }
+  );
+
+  if (response.ok) {
+    return response.json();
+  }
+  return Promise.reject({
+    msg: response.statusText,
+    status: response.status,
+  });
+};
+
+export const grantAdmin = async (values) => {
+  const response = await fetch("".concat(`${base_url}`, "/user/grantAdmin"), {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+    body: JSON.stringify(values, null, 2),
+  });
+
+  if (!response.ok) {
+    return Promise.reject({
+      msg: response.statusText,
+      status: response.status,
+    });
+  }
+};
